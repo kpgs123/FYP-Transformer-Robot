470ed6d1dfd94a34a268cd4873d040d7989cdc30
unknown <180349c@uom.lk>
2023-03-11 01:18:56 +0530

    Commit
    Code uploading for the github

---
+++ Transformation_Plotting/S shape.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+x = [8.5,8.5,25.5,25.5,-8.5,-8.5,-25.5,-25.5,8.5]
+y = [25.5,8.5,8.5,-8.5,-8.5,8.5,8.5,25.5,25.5]
+
+plt.figure()
+plt.plot(x,y)
+plt.show()
---
+++ Transformation_Plotting/python code forntransforming plotting/I to J transform.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# create data
+x = [8.5,-8.5,-8.5,-25.5]
+y = [-25.5,-25.5,8.5,8.5]
+
+x1 = np.linspace(-25.5, -32.5416, 1000)
+y1 = np.sqrt(17**2*2 -(x1 + 8.5)**2)-8.5
+
+x2 = np.linspace(8.5, -32.5416, 1000)
+y2 = -np.sqrt(17**2*2 -(x2 + 8.5)**2)-8.5
+
+
+
+# create a figure 
+plt.figure()
+
+# plot data on the axis
+plt.plot(x,y)
+plt.plot(x1,y1)
+plt.plot(x2,y2)
+
+# show the plot
+plt.show()
