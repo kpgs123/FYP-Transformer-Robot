9e073afba911660a5f6966dbc246ef61149b472e
unknown <180349c@uom.lk>
2023-03-15 16:24:24 +0530

    Changed
    Add Origin and change the x,y scale

--- Transformation_Plotting/I to O transform.py
+++ Transformation_Plotting/I to O transform.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 import numpy as np
 # create data
 x = [8.5,-8.5,-8.5,8.5]
@@ -16,16 +17,24 @@ y3 = np.sqrt(17**2*2 -(x3 + 8.5)**2) - 8.5
 x4 = np.linspace(8.5, -32.5416, 1000)
 y4 = -np.sqrt(17**2*2 -(x4 + 8.5)**2) - 8.5
 
+fig, ax = plt.subplots()
+ax.set_aspect('equal') # Set the aspect ratio to 1:1
 
-# create a figure 
-plt.figure()
+circle = Circle((0, 0), radius=1, edgecolor='red', facecolor='red')
+
+# Add the circle to the axes
+ax.add_artist(circle)
+
+# Set the axis limits
+ax.set_xlim(-50, 50)
+ax.set_ylim(-40, 60)
 
 # plot data on the axis
-plt.plot(x,y)
-plt.plot(x1,y1)
-plt.plot(x2,y2)
-plt.plot(x3,y3)
-plt.plot(x4,y4)
+ax.plot(x,y)
+ax.plot(x1,y1)
+ax.plot(x2,y2)
+ax.plot(x3,y3)
+ax.plot(x4,y4)
 
 # show the plot
 plt.show()
