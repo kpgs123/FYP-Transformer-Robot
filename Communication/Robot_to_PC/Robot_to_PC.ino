8ccebaec510e1489375670d8429f0784deb07ab0
unknown <sandamalkpg@gmail.com>
2023-03-16 21:52:32 +0530

    Working basic programs
    Working programs tested with the robot

--- Communication/Robot_to_PC/Robot_to_PC.ino
+++ Communication/Robot_to_PC/Robot_to_PC.ino
@@ -1,19 +1,76 @@
-#include <HardwareSerial.h>
+#include <smorphi.h>
 #include <BluetoothSerial.h>
 
 BluetoothSerial SerialBT;
 
-void setup() {
-  Serial.begin(9600);
-  SerialBT.begin("ESP32 Bluetooth Serial Port"); // set the Bluetooth name
-  Serial.println("Bluetooth Serial Port is ready.");
+Smorphi my_robot;
+
+void setup() {  
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT"); // Set the name for the Bluetooth device
+
+  my_robot.BeginSmorphi();
+
+
+  /*Serial.println("moving forward");
+  my_robot.MoveForward(10); // robot moves forward with speed 10 for 2 seconds
+  delay(2000);
+  my_robot.stopSmorphi();
+  delay(500);
+  Serial.println("transforming to l shape");
+  my_robot.L();
+  delay(5000);
+  my_robot.MoveBackward(10);
+  delay(2000);
+  my_robot.stopSmorphi();
+  my_robot.O();*/
+
+
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read()); // send serial data to Bluetooth
-  }
+  char x;
+
   if (SerialBT.available()) {
-    Serial.write(SerialBT.read()); // send Bluetooth data to serial
+    x = SerialBT.read();
+    if (x == 'O'){
+      my_robot.stopSmorphi();
+      delay(500);
+      Serial.println("transforming to o shape");
+      my_robot.O();
+      delay(5000);
+    }
+    else if (x == 'L'){
+      my_robot.stopSmorphi();
+      delay(500);
+      Serial.println("transforming to o shape");
+      my_robot.L();
+      delay(5000);
+    }
+    else if (x == 'S'){
+      my_robot.stopSmorphi();
+      delay(500);
+      Serial.println("transforming to o shape");
+      my_robot.S();
+      delay(5000);
+    }
+    else if (x == 'J'){
+      my_robot.stopSmorphi();
+      delay(500);
+      Serial.println("transforming to o shape");
+      my_robot.J();
+      delay(5000);
+    }
+    else if (x == 'I'){
+      my_robot.stopSmorphi();
+      delay(500);
+      Serial.println("transforming to o shape");
+      my_robot.I();
+      delay(5000);
+    }
+     SerialBT.write('R');
+     delay(100);
   }
+  
 }
