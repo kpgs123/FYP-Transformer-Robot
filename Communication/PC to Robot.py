449f28a4c0677ccb28baacce29afaf4d66dc7923
unknown <sandamalkpg@gmail.com>
2023-03-16 21:46:07 +0530

    Working basic programs
    Working programs tested with the robot

--- Communication/PC to Robot.py
+++ Communication/PC to Robot.py
@@ -2,20 +2,14 @@ import serial
 import time
 
 # Replace "/dev/tty.SLAB_USBtoUART" with the Bluetooth serial port of your ESP32
-ser = serial.Serial('COM4', 9600, timeout=1)
-
-# Send data from Python to ESP32
-data = "Hello, ESP32!"
-ser.write(data.encode())
+ser = serial.Serial('COM9', 9600, timeout=1)
 
 while True:
 # Receive data from ESP32 in Python
+    s = input()
+    data = s
+    ser.write(data.encode())
+    time.sleep(1)
     data = ser.readline()
     s = data.decode()
-    if s != "":
-        print(s, end="")
-        if s[:2] == "Hi":
-            data = "What is your name?"
-            ser.write(data.encode())
-
-    time.sleep(1)
+    print(s[:2])
