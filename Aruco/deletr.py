b14e86af296c654bc77f17def26e282bfee364bf
Dhilan Madhusanka <dilanmadusanka10@gmail.com>
2023-03-27 13:32:27 +0530

    -m

--- Aruco/deletr.py
+++ Aruco/deletr.py
@@ -1,13 +1,26 @@
+### Overlay marker ID and video
 import cv2
-from cv2 import aruco 
-import os
+from cv2 import aruco
+import time
 
-loc = r'C:\test'
+dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters_create()
 
-mark=2
-size = 20
+cap = cv2.VideoCapture('G:/sem 7/FYP/Git/FYP-Transformer-Robot/FYP Videos/2_res_Trim.mp4')
 
-output = aruco.Dictionary_get(aruco.DICT_4X4_50)
+try:
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
 
-print(output)
+        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+        cv2.imshow('frame', frame_markers)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyWindow('frame')
+    cap.release()
+except KeyboardInterrupt:
+    cv2.destroyWindow('frame')
+    cap.release()
