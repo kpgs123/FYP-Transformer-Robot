c93f736722b244a21e9753334c7a74bb7e2d41de
Dhilan Madhusanka <dilanmadusanka10@gmail.com>
2023-03-29 07:26:38 +0530

    all done with robot tracking, this outputs path matrix with tracked coordinates

--- Aruco/Detection_Aruco.py
+++ Aruco/Detection_Aruco.py
@@ -1,16 +1,17 @@
-### Overlay marker ID and video
 import cv2
 from cv2 import aruco
-import time
 import numpy as np
 
 dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters_create()
-url= 'http://10.10.2.198:8080/video'
+#url= 'http://10.10.2.198:8080/video'
+url=0
+not_detected = 0
+marker_not_detected_thersold = 60
+
 path = np.empty((0, 2), float)
 
-cap = cv2.VideoCapture('G:/sem 7/FYP/Git/FYP-Transformer-Robot/FYP Videos/2_res_Trim.mp4')
-#cap = cv2.VideoCapture(url)
+cap = cv2.VideoCapture(url)
 try:
     while True:
         ret, frame = cap.read()
@@ -18,22 +19,29 @@ try:
         # No more frames in video, break out of loop
         if not ret:
             break
-        
+
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        #cv2.imshow('frame2',gray)
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+        
+        if len(corners) > 0:
+            
+            centroid = np.mean(corners[0][0], axis=0)
+            path = np.append(path , np.array([centroid]), axis=0)
 
-        #print(corners)
-        centroid = np.mean(corners[0][0], axis=0)
-        #print(centroid)
-        path = np.append(path , np.array([centroid]), axis=0)
+            frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+            cv2.imshow('frame', frame_markers)
+            not_detected = 0
+        else:
+            not_detected = not_detected +1
+            cv2.imshow('frame', frame)
+        
+        if not_detected == marker_not_detected_thersold:
+            print("Error - cannot detected marker")
+            break
 
-
-
-        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-        cv2.imshow('frame', frame_markers)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     cv2.destroyWindow('frame')
     cap.release()
     print(path)
