023ace554b4426829a1a4e81075d3a299caf8aef
Dhilan Madhusanka <dilanmadusanka10@gmail.com>
2023-05-17 13:33:19 +0530

    Camera image

--- save_image.py
+++ save_image.py
@@ -1,7 +1,8 @@
 import cv2
 
 # Open the default camera (index 0)
-cap = cv2.VideoCapture(0)
+url = "rtsp://root:abcd@192.168.0.90/axis-media/media.amp?camera=1"
+cap = cv2.VideoCapture(url)
 
 # Check if the camera is opened correctly
 if not cap.isOpened():
