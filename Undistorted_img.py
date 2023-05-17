20c33cc40852fd03314919759f7bbd28fc26d504
Dhilan Madhusanka <dilanmadusanka10@gmail.com>
2023-05-17 14:36:08 +0530

    undistorted Camera image

--- Undistorted_img.py
+++ Undistorted_img.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 # Load the distorted image
-distorted_image = cv2.imread('/home/geethaka/Documents/Git/FYP-Transformer-Robot/distorted_image.jpg')
+distorted_image = cv2.imread('G:/sem 7/FYP/New Git/FYP-Transformer-Robot/camera_image.jpg')
 
-distorted_image = cv2.resize(distorted_image, (100, 75))
+#distorted_image = cv2.resize(distorted_image, (100, 75))
 
 # Define the camera matrix and distortion coefficients
-camera_matrix = np.load("/home/geethaka/Documents/Git/FYP-Transformer-Robot/2/camera_matrix.npy")
+camera_matrix = np.load("G:/sem 7/FYP/New Git/FYP-Transformer-Robot/4/camera_matrix.npy")
 
-distortion_coeffs = np.load("/home/geethaka/Documents/Git/FYP-Transformer-Robot/2/distortion_coeffs.npy")
+distortion_coeffs = np.load("G:/sem 7/FYP/New Git/FYP-Transformer-Robot/4/distortion_coeffs.npy")
 
 # Get the image size
 image_size = (distorted_image.shape[1], distorted_image.shape[0])
