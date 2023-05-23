ad86adc97832733b87e49bf3737f165b83b6e813
unknown <sandamalkpg@gmail.com>
2023-05-23 15:09:15 +0530

    added new environment pictures

---
+++ Navigation/Undistorted_img.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+# Load the distorted image
+distorted_image = cv2.imread('G:/sem 7/FYP/New Git/FYP-Transformer-Robot/pic/picture1.jpg')
+
+#distorted_image = cv2.resize(distorted_image, (100, 75))
+
+# Define the camera matrix and distortion coefficients
+camera_matrix = np.load("G:/sem 7/FYP/New Git/FYP-Transformer-Robot/CaliFinal/camera_matrix.npy")
+
+distortion_coeffs = np.load("G:/sem 7/FYP/New Git/FYP-Transformer-Robot/CaliFinal/distortion_coeffs.npy")
+
+# Get the image size
+image_size = (distorted_image.shape[1], distorted_image.shape[0])
+
+# Perform the undistortion
+undistorted_image = cv2.undistort(distorted_image, camera_matrix, distortion_coeffs)
+
+# Save the undistorted image
+cv2.imwrite('undistorted_image.jpg', undistorted_image)
+
+# Display the undistorted image
+cv2.imshow('Undistorted Image', undistorted_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
